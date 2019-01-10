2ce5938c9a08518ba476b752e9729686bf1963d8
Miguel Angel Bermeo <bermeom@github.com>
2019-01-10 13:49:39 +0100

    Exit condition added

--- envs/quadreped.py
+++ envs/quadreped.py
@@ -2,37 +2,70 @@ import numpy as np
 from gym import utils
 from os import path
 from gym.envs.mujoco import mujoco_env
+import math
 import os
+
+def mass_center(model, sim):
+    mass = np.expand_dims(model.body_mass, 1)
+    xpos = sim.data.xipos
+    return (np.sum(mass * xpos, 0) / np.sum(mass))[0]
 
 class QuadrepedEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         xmlpath = ((os.path.join(os.path.split(os.path.split(os.path.abspath(__file__))[0])[0], "xml/quadrepedrobot.xml")));
-        # print (xmlpath)
         mujoco_env.MujocoEnv.__init__(self, xmlpath, 5)
         utils.EzPickle.__init__(self)
 
     def step(self, action):
-        xposbefore = self.sim.data.qpos[0]
+        pos_before = mass_center(self.model, self.sim)
         self.do_simulation(action, self.frame_skip)
-        xposafter = self.sim.data.qpos[0]
+        pos_after = mass_center(self.model, self.sim)
+        alive_bonus = 5.0
+        data = self.sim.data
+        lin_vel_cost = 0.25 * (pos_after - pos_before) / self.model.opt.timestep
+        quad_ctrl_cost = 0.1 * np.square(data.ctrl).sum()
+        quad_impact_cost = .5e-6 * np.square(data.cfrc_ext).sum()
+        quad_impact_cost = min(quad_impact_cost, 10)
+        reward = lin_vel_cost - quad_ctrl_cost - quad_impact_cost + alive_bonus
+        qpos = self.sim.data.qpos
+        orientation = qpos.flat[3:7]; # w x y z
+        done = bool((math.fabs(orientation[1])+math.fabs(orientation[2]))>0.5) # rotational angles |x|+|y| 
+        print("Done : ",done," ",orientation)
+        # done = False;
         ob = self._get_obs()
-        reward_ctrl = - 0.1 * np.square(action).sum()
-        reward_run = (xposafter - xposbefore)/self.dt
-        reward = reward_ctrl + reward_run
-        done = False
-        return ob, reward, done, dict(reward_run=reward_run, reward_ctrl=reward_ctrl)
+        return ob, reward, done, dict(reward_linvel=lin_vel_cost, reward_quadctrl=-quad_ctrl_cost, reward_alive=alive_bonus, reward_impact=-quad_impact_cost)
+        # print("reward : ",dict(reward_linvel=lin_vel_cost, reward_quadctrl=-quad_ctrl_cost, reward_alive=alive_bonus, reward_impact=-quad_impact_cost));
+        # xposbefore = self.sim.data.qpos[0]
+        # self.do_simulation(action, self.frame_skip)
+        # xposafter = self.sim.data.qpos[0]
+        # ob = self._get_obs()
+        # reward_ctrl = - 0.1 * np.square(action).sum()
+        # reward_run = (xposafter - xposbefore)/self.dt
+        # reward = reward_ctrl + reward_run
+        # done = False
+        # return ob, reward, done, dict(reward_run=reward_run, reward_ctrl=reward_ctrl)
 
     def _get_obs(self):
         return np.concatenate([
-            self.sim.data.qpos.flat[1:],
-            self.sim.data.qvel.flat,
+                               self.sim.data.qpos.flat[1:],
+                               self.sim.data.qvel.flat,
+                               self.sim.data.cinert.flat,
+                               self.sim.data.cvel.flat,
+                               self.sim.data.qfrc_actuator.flat,
+                               self.sim.data.cfrc_ext.flat
         ])
 
     def reset_model(self):
-        qpos = self.init_qpos + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq)
-        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
-        self.set_state(qpos, qvel)
+        c = 0.01
+        self.set_state(
+            self.init_qpos + self.np_random.uniform(low=-c, high=c, size=self.model.nq),
+            self.init_qvel + self.np_random.uniform(low=-c, high=c, size=self.model.nv,)
+        )
         return self._get_obs()
 
     def viewer_setup(self):
-        self.viewer.cam.distance = self.model.stat.extent * 0.5
+        # self.viewer.cam.distance = self.model.stat.extent * 0.5
+        self.viewer.cam.trackbodyid = 1
+        self.viewer.cam.distance = self.model.stat.extent * 1.0
+        self.viewer.cam.lookat[2] = 2.0
+        self.viewer.cam.elevation = -20
--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 Shows how to toss a capsule to a container.
 """
 import numpy as np
+import math
 from pprint import pprint
 import mujoco_py as mpy #import load_model_from_path, MjSim, MjViewer
 import os
+
+   
 model = mpy.mujoco_py.load_model_from_path("xml/quadrepedrobot.xml")
 # model = mpy.mujoco_py.load_model_from_path("xml/humanoid.xml")
 sim = mpy.MjSim(model)
@@ -30,22 +33,35 @@ perp = mpy.cymj.PyMjvPerturb();
 # print("start",qfrc_target);
 sw = True;
 while True:
+    print ("0 - orientation -> body_quat ",sim.data.qpos.flat[3:7])
+    # print ("0 - orientation -> body_quat ",sim.data.qpos.flat[0:7])
+    mat = np.zeros(9, dtype=np.float64)
+    mpy.functions.mju_quat2Mat(mat, sim.data.body_xquat[2]);
+    # print ("0 - orientation -> ",mat)
+    # print("0 - data -> ",sim.data.qM)
+    
+    # sw = False;
     sim.set_state(sim_state)
-    print("qfrc_applied[0]-> ",sim.data.qfrc_applied);
-    # force[1]=150;
-    # # mpy.functions.mj_applyFT(sim.model,sim.data,force,torque,point,body,sim.data.qfrc_applied);
+    print ("0 - orientation -> body_quat ",sim.data.qpos.flat[3:7])
+    mat = np.zeros(9, dtype=np.float64)
+    mpy.functions.mju_quat2Mat(mat, sim.data.body_xquat[2]);
+    # print ("orientation -> ",mat)
+    # print("data -> ",sim.data.qM)
+    # print("qfrc_applied[0]-> ",sim.data.qfrc_applied);
+    force[1]=-150;
+    # mpy.functions.mj_applyFT(sim.model,sim.data,force,torque,point,body,sim.data.qfrc_applied);
     sim.step();
     # print("qfrc_applied[1]-> ",sim.data.qfrc_applied);
-    # # mpy.functions.mj_applyFT(sim.model,sim.data,-sim.data.qfrc_applied,torque,point,body,sim.data.qfrc_applied);
-    # if sw :
-    #     force[1]=0;
-    # sw =  not sw;
+    # mpy.functions.mj_applyFT(sim.model,sim.data,-sim.data.qfrc_applied,torque,point,body,sim.data.qfrc_applied);
+    if sw :
+        force[1]=0;
+    sw =  not sw;
     # sim.step();
     # sim.data.qfrc_applied[0]=0;
     # mpy.functions.mjv_applyPerturbForce(sim.model,sim.data,perp);
     # print("qfrc_applied[2]-> ",sim.data.qfrc_applied);
-    pprint(sim.data)
-    pprint(sim.data.qpos.flat)   
+    
+    # pprint(sim.data.qpos.flat)   
 
     # sim.data.ctrl[4*0+3]=-1;
     # sim.data.ctrl[4*1+3]=-1;
@@ -53,10 +69,11 @@ while True:
     # sim.data.ctrl[4*3+3]=-1;
     sim.step();
     observations = np.concatenate([
-            sim.data.qpos.flat,
-            sim.data.qvel.flat,
+            sim.data.qpos,
+            sim.data.qvel,
         ]);
-    print(len(sim.data.qpos)," ",len(sim.data.qvel),"\n",observations);
+    orientation = sim.data.qpos.flat[3:7];
+    # print(len(sim.data.qpos)," ",len(sim.data.qvel),"\n",observations);
     for i in range(500):
         if i < 200 | i > 800 :
             sim.data.ctrl[3]=1;
@@ -72,13 +89,22 @@ while True:
             sim.data.ctrl[k+3*4] = -1.0/2
         sim.step();
         viewer.render();
+        orientation = sim.data.qpos.flat[3:7]; # w x y z
+        if (orientation[1]+orientation[2]>0.5):
+            break;
+
         # mpy.functions.mj_applyFT(sim.model,sim.data,force0,torque,point,body,qfrc_target);
         # print("qfrc_target",qfrc_target);
     observations = np.concatenate([
         sim.data.qpos.flat[1:],
         sim.data.qvel.flat,
     ]);
-    print("END",len(sim.data.qpos)," ",len(sim.data.qvel),"\n",observations);
-
+    # print("END",len(sim.data.qpos)," ",len(sim.data.qvel),"\n",observations);
+    # mat = np.zeros(9, dtype=np.float64)
+    # mpy.functions.mju_quat2Mat(mat, sim.data.body_xquat[2]);
+    # print ("orientation -> ",mat)
+    # print("data -> ",sim.data.qM)
+    
+    
     if os.getenv('TESTING') is not None:
         break
