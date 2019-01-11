cfea2c5911fc97eb7e2a0c8b99d515bd12dda75b
Miguel Angel Bermeo Ayerbe <bermeom@javeriana.edu.co>
2019-01-11 21:50:33 +0100

    Nets trained with 260 episodes

--- learning/scripts/script_9_quadruped_DDPG.sh
+++ learning/scripts/script_9_quadruped_DDPG.sh
@@ -1,1 +1,2 @@
+#python3 ./execute.py reinforcement source_quadruped_robot player_DDPG_1A --load quadruped_robot_DDPG_1A --save quadruped_robot_DDPG_1A_1 --epis 50000
 python3 ./execute.py reinforcement source_quadruped_robot player_DDPG_1A --save quadruped_robot_DDPG_1A --epis 50000
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='quadreped-robot',
-      version='0.0.1',
-      install_requires=['gym',
-                        'mujoco-py>=1.50.1']
-)
